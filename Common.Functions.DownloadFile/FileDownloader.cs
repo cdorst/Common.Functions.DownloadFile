99496737d59e829f4eedd5a2a3015b32483111dc
Christopher Dorst <chris@dorst.email>
2017-12-29 07:12:33 -0600

    Add FileDownloader

---
+++ Common.Functions.DownloadFile/FileDownloader.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace Common.Functions.DownloadFile
+{
+    public static class FileDownloader
+    {
+        public static void Download(string uri, string destinationFilePath)
+        {
+            using (var client = new WebClient())
+                client.DownloadFile(uri, destinationFilePath);
+        }
+    }
+}
