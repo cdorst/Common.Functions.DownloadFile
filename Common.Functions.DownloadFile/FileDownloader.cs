2554b9dd1885f4500572bf4b925717c0a04c44e0
Christopher Dorst <chris@dorst.email>
2017-12-30 21:42:07 -0600

    Preferring strongly-typed Uri structure over string

--- Common.Functions.DownloadFile/FileDownloader.cs
+++ Common.Functions.DownloadFile/FileDownloader.cs
@@ -1,10 +1,11 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace Common.Functions.DownloadFile
 {
     public static class FileDownloader
     {
-        public static void Download(string uri, string destinationFilePath)
+        public static void Download(Uri uri, string destinationFilePath)
         {
             using (var client = new WebClient())
                 client.DownloadFile(uri, destinationFilePath);
